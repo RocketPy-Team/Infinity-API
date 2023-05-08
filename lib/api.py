3eeba3c525aa7cccaec87297d816a943a6d976ee
GabrielBarberini <gabriel.barberini@sumup.com>
2023-05-08 13:18:52 -0300

    Refactor to MVC, implements mongoDB client and hashed flight id's
    pydantize return objects
    
    updates README
    
    updates api routes and models
    
    adds repositories
    
    implements repo CRUD
    
    fixes pydantic views
    
    updates smoke testing suite
    
    fixes repo create
    
    implements mongo calls
    
    updates smoke test exmaples
    
    fixes delete repo
    
    refactor controller and repo
    
    refactors api+controllers+repo
    
    refactores api+controllers+repositories
    
    TBD: disclaimer on updates
    
    refactors controllers api and repos
    
    fixes mongoDB CRUD
    
    fixes simulation summary
    
    fixes simulation attempt on unexisting flight
    
    deduces flight id by hashing
    
    implements hashing id's for CRUD
    
    minor spacing change

--- lib/api.py
+++ lib/api.py
@@ -1,10 +1,13 @@
 # api.py
 
-from fastapi import FastAPI, Response, status
+from fastapi import FastAPI, Response, status, Header
 from fastapi.middleware.cors import CORSMiddleware
 
+from lib.views import FlightSummary
 from lib.models import Env, Flight, Rocket
 from lib.controllers import EnvController, FlightController
+
+from typing import Any
 
 app = FastAPI()
 app.add_middleware(
@@ -15,45 +18,43 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-# Simulation CRUD 
-## Create simulation by flight
-@app.post("/simulation/flight/")
-async def flight_simulation(flight: Flight):
-    flight_controller = FlightController(flight)
-    flight_summary = flight_controller.summary('full')
-    return flight_summary
+# Flight CRUD 
+## Create flight
+@app.post("/flight/")
+async def create_flight(flight: Flight) -> dict[str, str]:
+    successfully_created_flight = FlightController(flight).create_flight()
+    return successfully_created_flight
 
-## Create simulation by environment
-@app.post("/simulation/env/")
-async def environment_simulation(env: Env):
-    flight_controller = FlightController( Flight(environment=env) )
-    flight_summary = flight_controller.summary('full')
-    return flight_summary
+## Read flight 
+@app.get("/flight/")
+async def read_flight(flight_id: int) -> Flight:
+    return FlightController.get_flight(flight_id)
 
-## Read simmulation
-@app.get("/simulation/{id}")
-async def read_simulation(id: str):
-    return "Coming soon 2!" 
+## Update flight environment 
+@app.put("/flight/env/")
+async def update_flight_env(flight_id: int, env: Env) -> dict[str, Any]:
+    return FlightController.update_env(flight_id, env)
 
-## Update simulation environment 
-@app.put("/simulation/env/{id}")
-async def update_simulation_env(id: str, env: Env):
-    return "Coming soon!" 
+## Update flight rocket 
+@app.put("/flight/rocket/")
+async def update_flight_rocket(flight_id: int, rocket: Rocket) -> dict[str, Any]:
+    return FlightController.update_rocket(flight_id, rocket)
 
-## Update simulation rocket 
-@app.put("/simulation/rocket/{id}")
-async def update_simulation_rocket(id: str, rocket: Rocket):
-    return "Coming soon!" 
+## Update flight 
+@app.put("/flight/")
+async def update_flight(flight_id: int, flight: Flight) -> dict[str, Any]:
+    return FlightController(flight).update_flight(flight_id)
 
-## Update simulation flight 
-@app.put("/simulation/flight/{id}")
-async def update_simulation_flight(id: str, flight: Flight):
-    return "Coming soon!" 
+## Delete flight 
+@app.delete("/flight/")
+async def delete_flight(flight_id: int) -> dict[str, str]:
+    return FlightController.delete_flight(flight_id)
 
-## Delete simulation
-@app.delete("/simulation/{id}")
-async def delete_simulation(id: str):
-    return "Coming soon!"
+# Simmulation
+## Simulate flight
+@app.post("/flight/simulate/")
+async def simulate_flight(flight_id: int) -> FlightSummary:
+    return FlightController.simulate(flight_id)
 
 # Check app health
 @app.get("/health", status_code=status.HTTP_200_OK)
