b95b1f07c3e1c233bee9462478ee81e5d67bc532
GabrielBarberini <gabriel.barberini@sumup.com>
2023-05-08 13:18:52 -0300

    Refactor to MVC, implements mongoDB client and hashed flight id's
    pydantize return objects
    
    updates README
    
    updates api routes and models
    
    adds repositories
    
    implements repo CRUD
    
    fixes pydantic views
    
    updates smoke testing suite
    
    fixes repo create
    
    implements mongo calls
    
    updates smoke test exmaples
    
    fixes delete repo
    
    refactor controller and repo
    
    refactors api+controllers+repo
    
    refactores api+controllers+repositories
    
    TBD: disclaimer on updates
    
    refactors controllers api and repos
    
    fixes mongoDB CRUD
    
    fixes simulation summary
    
    fixes simulation attempt on unexisting flight
    
    deduces flight id by hashing
    
    implements hashing id's for CRUD
    
    minor spacing change

---
+++ lib/repositories.py
@@ -0,0 +1,76 @@
+from pymongo import MongoClient
+from pymongo.results import InsertOneResult
+from pymongo.results import DeleteResult
+from lib.models import Flight
+
+class Repository:
+    _self = None
+
+    def __new__(cls, *args, **kwargs):
+        if cls._self is None:
+            cls._self = super().__new__(cls)
+        return cls._self
+
+    def __init__(self):
+        self.connection_string = "***REMOVED***"
+        self.client = MongoClient(self.connection_string)
+        self.db = self.client.rocketpy
+        self.collection = self.db.flights
+
+    def __del__(self):
+        self.client.close()
+
+class FlightRepository(Repository):
+        
+    def __init__(self, flight: Flight = None, flight_id: str = None):
+        super().__init__()
+        self.flight = flight
+        if flight_id:
+            self.flight_id = flight_id
+        else:
+            self.flight_id = self.flight.__hash__()
+
+    def __del__(self):
+        super().__del__()
+
+    def create_flight(self) -> InsertOneResult:
+        if not self.get_flight():
+            try: 
+                flight_to_dict = self.flight.dict()
+                flight_to_dict["flight_id"] = self.flight_id 
+                return self.collection.insert_one(flight_to_dict)
+            except:
+                raise Exception("Error creating flight")
+        return InsertOneResult( acknowledged=True, inserted_id=None )
+
+    def update_flight(self) -> tuple | None:
+        try:
+            flight_to_dict = self.flight.dict()
+            flight_to_dict["flight_id"] = self.flight.__hash__() 
+
+            updated_flight = self.collection.update_one(
+                { "flight_id": self.flight_id }, 
+                { "$set": flight_to_dict }
+            )
+
+            self.flight_id = flight_to_dict["flight_id"]
+            return  self.flight_id, self.get_flight()
+        except:
+            raise Exception("Error updating flight")
+
+    def get_flight(self) -> Flight | None:
+        try:
+            flight = self.collection.find_one({ "flight_id": self.flight_id })
+            if flight is not None:
+                del flight["_id"] 
+                return Flight.parse_obj(flight)
+            else:
+                return None
+        except:
+            raise Exception("Error getting flight")
+    
+    def delete_flight(self) -> DeleteResult: 
+        try: 
+            return self.collection.delete_one({ "flight_id": self.flight_id })
+        except:
+            raise Exception("Error deleting flight")
