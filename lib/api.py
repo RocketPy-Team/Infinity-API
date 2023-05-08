f259aa7f72aeba212ca272cf3ed30bef32d8319f
GabrielBarberini <gabriel.barberini@sumup.com>
2023-05-08 11:33:33 -0300

    structure simulation CRUD

--- lib/api.py
+++ lib/api.py
@@ -15,29 +15,45 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-# Environment
-@app.post("/simulation/env/")
-async def simulate_env(env: Env):
-    flight_controller = FlightController( Flight(environment=env) )
-    flight_summary = flight_controller.summary('full')
-    return flight_summary
-
-# Flight
+# Simulation CRUD 
+## Create simulation by flight
 @app.post("/simulation/flight/")
-async def simulate_flight(flight: Flight):
+async def flight_simulation(flight: Flight):
     flight_controller = FlightController(flight)
     flight_summary = flight_controller.summary('full')
     return flight_summary
 
-# Rocket
-@app.post("/simulation/rocket/")
-async def simulate_rocket(rocket: Rocket):
-    return "Coming soon!" 
+## Create simulation by environment
+@app.post("/simulation/env/")
+async def environment_simulation(env: Env):
+    flight_controller = FlightController( Flight(environment=env) )
+    flight_summary = flight_controller.summary('full')
+    return flight_summary
 
-# Read simmulation
+## Read simmulation
 @app.get("/simulation/{id}")
 async def read_simulation(id: str):
     return "Coming soon 2!" 
+
+## Update simulation environment 
+@app.put("/simulation/env/{id}")
+async def update_simulation_env(id: str, env: Env):
+    return "Coming soon!" 
+
+## Update simulation rocket 
+@app.put("/simulation/rocket/{id}")
+async def update_simulation_rocket(id: str, rocket: Rocket):
+    return "Coming soon!" 
+
+## Update simulation flight 
+@app.put("/simulation/flight/{id}")
+async def update_simulation_flight(id: str, flight: Flight):
+    return "Coming soon!" 
+
+## Delete simulation
+@app.delete("/simulation/{id}")
+async def delete_simulation(id: str):
+    return "Coming soon!"
 
 # Check app health
 @app.get("/health", status_code=status.HTTP_200_OK)
