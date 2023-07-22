db018e4f224a29e433eef7e0526898778afff51d
GabrielBarberini <gabriel.barberini@sumup.com>
2023-07-22 17:11:25 -0300

    modularize controllers

---
+++ lib/controllers/motor.py
@@ -0,0 +1,28 @@
+class MotorController():
+    """
+    Controller for the motor model.
+
+    Init Attributes:
+        motor (models.Motor): Motor model object.
+
+    Enables:
+        - Create a rocketpy.Motor object from a Motor model object.
+    """
+    def __init__(self, motor: Motor):
+        rocketpy_motor = SolidMotor(
+                burnOut=motor.burnOut,
+                grainNumber=motor.grainNumber,
+                grainDensity=motor.grainDensity,
+                grainOuterRadius=motor.grainOuterRadius,
+                grainInitialInnerRadius=motor.grainInitialInnerRadius,
+                grainInitialHeight=motor.grainInitialHeight,
+                grainsCenterOfMassPosition=-motor.grainsCenterOfMassPosition,
+                thrustSource=motor.thrustSource
+        )
+        rocketpy_motor.grainSeparation = motor.grainSeparation
+        rocketpy_motor.nozzleRadius = motor.nozzleRadius
+        rocketpy_motor.throatRadius = motor.throatRadius
+        rocketpy_motor.interpolationMethod = motor.interpolationMethod
+        self.rocketpy_motor = rocketpy_motor
+        self.motor = motor
+
