aced96153d148acfeba3a07286c081b7123fa29e
GabrielBarberini <gabriel.barberini@sumup.com>
2023-09-09 20:26:47 -0300

    fixes message typo

--- lib/controllers/environment.py
+++ lib/controllers/environment.py
@@ -109,7 +109,7 @@ class EnvController():
 
         if successfully_updated_env:
             return { 
-                    "message": "Environment successfully updated ", 
+                    "message": "Environment successfully updated", 
                     "new_env_id": str(successfully_updated_env)
             }
         else:
@@ -134,7 +134,7 @@ class EnvController():
 
         successfully_deleted_env = EnvRepository(env_id=env_id).delete_env()
         if successfully_deleted_env: 
-            return {"deleted_env_id": str(env_id), "message": "Environment successfully deleted "}
+            return {"deleted_env_id": str(env_id), "message": "Environment successfully deleted"}
         else:
             return Response(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
