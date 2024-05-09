9eac5dca059b2bc43c205f7a53974f6888da5169
GabrielBarberini <gabriel.barberini@sumup.com>
2024-05-09 00:23:06 -0300

    enhances error log parsing

--- lib/controllers/__init__.py
+++ lib/controllers/__init__.py
@@ -1,6 +1,7 @@
 # lib/controllers/__init__.py
 
 
-def parse_error(e):
-    exc_str = f"{e}".replace("\n", " ").replace("   ", " ")
-    return exc_str
+def parse_error(error):
+    exc_type = type(error).__name__
+    exc_obj = f"{error}".replace("\n", " ").replace("   ", " ")
+    return f"{exc_type} exception: {exc_obj}"
