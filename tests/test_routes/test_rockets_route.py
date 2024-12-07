e905c034eebfc3080116dbd5adf23e7a1c0ad42b
Gabriel Barberini <gabriel.barberini@sumup.com>
2024-12-07 08:27:09 -0300

    implements flight route tests; bring common fixturest o toplevel conftest

--- tests/test_routes/test_rockets_route.py
+++ tests/test_routes/test_rockets_route.py
@@ -14,9 +14,6 @@ from lib.models.rocket import Rocket
 from lib.models.motor import (
     Motor,
     MotorKinds,
-    MotorTank,
-    TankFluids,
-    TankKinds,
 )
 from lib.controllers.rocket import RocketController
 from lib.views.motor import MotorView
@@ -30,71 +27,6 @@ from lib.views.rocket import (
 from lib import app
 
 client = TestClient(app)
-
-
-@pytest.fixture
-def stub_motor():
-    motor = Motor(
-        thrust_source=[[0, 0]],
-        burn_time=0,
-        nozzle_radius=0,
-        dry_mass=0,
-        dry_inertia=[0, 0, 0],
-        center_of_dry_mass_position=0,
-    )
-    motor_json = motor.model_dump_json()
-    return json.loads(motor_json)
-
-
-@pytest.fixture
-def stub_tank():
-    tank = MotorTank(
-        geometry=[[(0, 0), 0]],
-        gas=TankFluids(name='gas', density=0),
-        liquid=TankFluids(name='liquid', density=0),
-        flux_time=(0, 0),
-        position=0,
-        discretize=0,
-        name='tank',
-    )
-    tank_json = tank.model_dump_json()
-    return json.loads(tank_json)
-
-
-@pytest.fixture
-def stub_level_tank(stub_tank):
-    stub_tank.update({'tank_kind': TankKinds.LEVEL, 'liquid_height': 0})
-    return stub_tank
-
-
-@pytest.fixture
-def stub_mass_flow_tank(stub_tank):
-    stub_tank.update(
-        {
-            'tank_kind': TankKinds.MASS_FLOW,
-            'gas_mass_flow_rate_in': 0,
-            'gas_mass_flow_rate_out': 0,
-            'liquid_mass_flow_rate_in': 0,
-            'liquid_mass_flow_rate_out': 0,
-            'initial_liquid_mass': 0,
-            'initial_gas_mass': 0,
-        }
-    )
-    return stub_tank
-
-
-@pytest.fixture
-def stub_ullage_tank(stub_tank):
-    stub_tank.update({'tank_kind': TankKinds.ULLAGE, 'ullage': 0})
-    return stub_tank
-
-
-@pytest.fixture
-def stub_mass_tank(stub_tank):
-    stub_tank.update(
-        {'tank_kind': TankKinds.MASS, 'liquid_mass': 0, 'gas_mass': 0}
-    )
-    return stub_tank
 
 
 @pytest.fixture
@@ -169,23 +101,6 @@ def stub_parachute():
     )
     parachute_json = parachute.model_dump_json()
     return json.loads(parachute_json)
-
-
-@pytest.fixture
-def stub_rocket(stub_motor):
-    rocket = Rocket(
-        motor=stub_motor,
-        radius=0,
-        mass=0,
-        motor_position=0,
-        center_of_mass_without_motor=0,
-        inertia=[0, 0, 0],
-        power_off_drag=[(0, 0)],
-        power_on_drag=[(0, 0)],
-        coordinate_system_orientation='TAIL_TO_NOSE',
-    )
-    rocket_json = rocket.model_dump_json()
-    return json.loads(rocket_json)
 
 
 def test_create_rocket(stub_rocket):
@@ -466,9 +381,9 @@ def test_create_rocket_server_error(stub_rocket):
 
 
 def test_read_rocket(stub_rocket, stub_motor):
+    del stub_rocket['motor']
     motor_view = MotorView(**stub_motor, selected_motor_kind=MotorKinds.HYBRID)
-    stub_rocket.update(motor=motor_view)
-    rocket_view = RocketView(**stub_rocket)
+    rocket_view = RocketView(**stub_rocket, motor=motor_view)
     with patch.object(
         RocketController,
         'get_rocket_by_id',
