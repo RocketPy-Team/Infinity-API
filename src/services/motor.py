5613933ed268b3aac589a8d3c131bf70484a6f5f
Gabriel Barberini <gabriel.barberini@sumup.com>
2025-11-01 14:06:56 +0100

    improve grain parsing feedback

--- src/services/motor.py
+++ src/services/motor.py
@@ -14,6 +14,8 @@ from rocketpy import (
     TankGeometry,
 )
 
+from fastapi import HTTPException, status
+
 from src.models.sub.tanks import TankKinds
 from src.models.motor import MotorKinds, MotorModel
 from src.views.motor import MotorSimulation
@@ -35,6 +37,12 @@ class MotorService:
             MotorService containing the rocketpy motor object.
         """
 
+        reshape_thrust_curve = motor.reshape_thrust_curve
+        if isinstance(reshape_thrust_curve, bool):
+            reshape_thrust_curve = False
+        elif isinstance(reshape_thrust_curve, list):
+            reshape_thrust_curve = tuple(reshape_thrust_curve)
+
         motor_core = {
             "thrust_source": motor.thrust_source,
             "burn_time": motor.burn_time,
@@ -44,7 +52,7 @@ class MotorService:
             "center_of_dry_mass_position": motor.center_of_dry_mass_position,
             "coordinate_system_orientation": motor.coordinate_system_orientation,
             "interpolation_method": motor.interpolation_method,
-            "reshape_thrust_curve": False or motor.reshape_thrust_curve,
+            "reshape_thrust_curve": reshape_thrust_curve,
         }
 
         match MotorKinds(motor.motor_kind):
@@ -63,15 +71,36 @@ class MotorService:
                     grains_center_of_mass_position=motor.grains_center_of_mass_position,
                 )
             case MotorKinds.SOLID:
+                grain_params = {
+                    'grain_number': motor.grain_number,
+                    'grain_density': motor.grain_density,
+                    'grain_outer_radius': motor.grain_outer_radius,
+                    'grain_initial_inner_radius': motor.grain_initial_inner_radius,
+                    'grain_initial_height': motor.grain_initial_height,
+                    'grain_separation': motor.grain_separation,
+                    'grains_center_of_mass_position': motor.grains_center_of_mass_position,
+                }
+
+                missing = [
+                    key for key, value in grain_params.items() if value is None
+                ]
+                if missing:
+                    raise HTTPException(
+                        status_code=status.HTTP_422_UNPROCESSABLE_ENTITY,
+                        detail=(
+                            "Solid motor requires grain configuration: missing "
+                            + ', '.join(missing)
+                        ),
+                    )
+
+                optional_params = {}
+                if motor.throat_radius is not None:
+                    optional_params['throat_radius'] = motor.throat_radius
+
                 rocketpy_motor = SolidMotor(
                     **motor_core,
-                    grain_number=motor.grain_number,
-                    grain_density=motor.grain_density,
-                    grain_outer_radius=motor.grain_outer_radius,
-                    grain_initial_inner_radius=motor.grain_initial_inner_radius,
-                    grain_initial_height=motor.grain_initial_height,
-                    grains_center_of_mass_position=motor.grains_center_of_mass_position,
-                    grain_separation=motor.grain_separation,
+                    **grain_params,
+                    **optional_params,
                 )
             case _:
                 rocketpy_motor = GenericMotor(
@@ -84,7 +113,7 @@ class MotorService:
                 )
 
         if motor.motor_kind not in (MotorKinds.SOLID, MotorKinds.GENERIC):
-            for tank in motor.tanks:
+            for tank in motor.tanks or []:
                 tank_core = {
                     "name": tank.name,
                     "geometry": TankGeometry(
